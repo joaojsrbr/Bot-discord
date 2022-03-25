cbdcaba548c555f541ca1388f23ac74d43eb2f86
João Vitor <joaovitor.jsr@gmail.com>
2022-03-25 18:39:26 -0300

    ...

--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
 from urllib.request import urlopen, Request
-from xmlrpc.client import Boolean
 import discord 
 from discord.ext import tasks, commands
 import json
-from re import I
 from urllib.request import urlopen, Request
-from xmlrpc.client import Boolean
 from bs4 import BeautifulSoup
 
+urln = "https://neoxscans.net/wp-content/uploads/2021/05/cropped-neoxscans-270x270.png"
+
+#Crie um arquivo config.json
 with open('config.json') as f:
     data = json.load(f)
     token = data["TOKEN"]
     prefix = data["PREFIX"]
     url = data["URL"]    
+    User_Agent = data["USER_AGENT"]
 
 bot = commands.Bot(prefix)
 
 @bot.event
 async def on_ready():
         print("Ready")
-        current_manga.start()
+        #current_manga.start()
 
 @bot.command(name="list")
 async def send_lastlist(ctx):
                 
         
-        headers ={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
+        headers = {"User-Agent": User_Agent}
         req = Request(url,headers=headers)
         response = urlopen(req)
         html = response.read()
@@ -35,27 +36,105 @@ async def send_lastlist(ctx):
         m= 3
 
         for i in range(0,int (m)):
-        
-                tabsitens =soup.find_all('div', class_="row c-tabs-item__content")
+                
+
+                #Scraping lista de tudo da página
+                tabsitens = soup.find_all('div', class_="row c-tabs-item__content")
                 itens = tabsitens[i]
-
+                
+                #Scraping Titulo
                 titulo = itens.find_next('h3', class_="h4", ).get_text().strip()
-                        
-                link = itens.find_next(href=True)
 
-        
-                titulo2= titulo
+                #Scraping Manga-cap 
+                linkk = itens.find_next('span', class_="font-meta chapter")
+                link = linkk.find_next(href=True)
 
-                link2 = link['href']
+                
+                #Scraping Capitulo 
+                cap = itens.find_next('span', class_="font-meta chapter" ).get_text().strip()
 
-                response =  titulo2 + "  -  " + link2
-                await ctx.send(response)
+                #Scraping link-manga
+                links =  itens.find_next('div', class_="tab-thumb c-image-hover").a
+                
+
+                ########################## ########################## ##########################
+                req1 = Request(links['href'],headers=headers)
+                response1 = urlopen(req1)
+                html1 = response1.read()
+                soup1 = BeautifulSoup(html1,'html.parser')
+                ########################## ########################## ##########################
+                
+                #Scraping urlFt     
+                fta = soup1.find('div', class_="tab-summary")
+                ftb = fta.find('div', class_='summary_image').a.img
+                
+
+                #Scraping Gênero
+                genre = soup1.find('div', class_="genres-content").get_text( ).lstrip()
+
+                #Scraping Tipo
+                tipo3cc = soup1.find('div', class_="post-title").span.get_text().lstrip()
+                
+
+                #Scraping Rank
+                rank = soup1.find('div', class_="post-rating").span.get_text().lstrip()
+            
+           
+                    
+                ########################## ########################## ########################## ########################## ##########################          
+
+                embed = discord.Embed(
+                        title = "```"+ titulo + "```", 
+                        description = "",
+                        color = 0x0000FF
+
+                )
+                
+               
+                embed.add_field(
+                 name="```Gênero```",
+                 value= "" + genre, 
+                 inline= True
+                 
+                 )
+                 
+                embed.add_field(
+                 name="```Tipo```",
+                 value=tipo3cc, 
+                 inline= False)
+
+                embed.add_field(
+                        name="```Cap```", 
+                        value=cap, 
+                        inline= True)
+
+                embed.add_field(
+                        name="```Rank```", 
+                        value=rank, 
+                        inline= True)
+
+                embed.add_field(
+                        name="```Link```", 
+                        value=link['href'], 
+                        inline= False )
+
+                embed.set_footer()
+
+                embed.set_author(
+                name="Neox Scanlator",
+                icon_url=urln)
+                embed.set_image(
+                        url=ftb['data-src']
+                )
+                
+              
+                await ctx.send(embed=embed)
 
 @tasks.loop(minutes=20)
 async def current_manga():
                 
         
-        headers ={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
+        headers = {"User-Agent": User_Agent}        
         req = Request(url,headers=headers)
         response = urlopen(req)
         html = response.read()
